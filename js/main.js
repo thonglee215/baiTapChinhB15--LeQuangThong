ea0db276c3400f1f521138c38d56c226e33b3b90
thonglee215 <87985272+thonglee215@users.noreply.github.com>
2023-02-13 09:56:20 +0700

    First update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//  BÀI 1
+//  Khối 1: Input
+//       day   
+   
+//  Khối 2: Các bước xử lý
+//       B1: Lấy giá trị từ form khi người dùng click button
+//          + Tạo hàm tongLuong
+//       B2: Trong hàm tongLuong
+//          + DOM tới thẻ cần lấy dữ liệu của form và lấy value
+//       B3: Lập công thức
+//          total = 10000 * day
+  
+//  Khối 3: Thông báo output
+//          total
